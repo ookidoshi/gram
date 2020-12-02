49f479a9b170382f1d1eaf65dffe3fd7114fa719
ookidoshi <okidookido@gmail.com>
2020-12-02 11:14:52 +0900

    relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
+  has_many :photos, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
